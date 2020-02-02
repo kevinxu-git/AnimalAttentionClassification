9b25376bd0f84f5bdaf61461a2bb2b7530854c75
Kevin XU <kevin-xu@hotmail.fr>
2020-02-03 00:23:08 +0100

    update baseline

--- submissions/starting_kit/image_classifier.py
+++ submissions/starting_kit/image_classifier.py
@@ -45,30 +45,30 @@ class ImageClassifier(object):
         
         model = Sequential()
 	
-	# Initialize layers
-        conv_layer1 = Conv2D(filters = 8, kernel_size = 5, activation='relu', input_shape=(360,640,3))
-	conv_layer2 = Conv2D(filters = 16, kernel_size = 3, activation='relu')
+	   # Initialize layers
+        conv_layer1 = Conv2D(filters = 8, kernel_size = 5, activation='relu', input_shape=(128,128,3))
+        conv_layer2 = Conv2D(filters = 16, kernel_size = 3, activation='relu')
 
-	maxpool_layer = MaxPooling2D(pool_size=2)
-	flatten_layer = Flatten()
+        maxpool_layer = MaxPooling2D(pool_size=2)
+        flatten_layer = Flatten()
 
-	dense_layer1 = Dense(10,activation = 'relu')
-	dense_layer2 = Dense(1,activation = 'sigmoid')
+        dense_layer1 = Dense(10,activation = 'relu')
+        dense_layer2 = Dense(2,activation = 'softmax')
 
         # Add them to the model
-	model.add(conv_layer1)
-	model.add(conv_layer2)
+        model.add(conv_layer1)
+        model.add(conv_layer2)
 
-	model.add(maxpool_layer)
-	model.add(flatten_layer)
+        model.add(maxpool_layer)
+        model.add(flatten_layer)
 
-	model.add(dense_layer1)
-	model.add(dense_layer2)
+        model.add(dense_layer1)
+        model.add(dense_layer2)
 
     	# Compile the model
-        model.compile(loss='categorical_crossentropy',
+        model.compile(loss='binary_crossentropy',
               optimizer=optimizers.Adam(lr=0.01),
               metrics=['accuracy'])
-        
+
         return model
 
